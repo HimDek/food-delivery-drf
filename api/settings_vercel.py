02d017fe84be62be24c7408ea7c91371bb71b529
Himprakash Deka <himprakashd@gmail.com>
2024-07-18 11:37:44 +0530

    fix

--- api/settings_vercel.py
+++ api/settings_vercel.py
@@ -4,4 +4,4 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = [".vercel.app", "now.sh". ".barakbits.com"]
+ALLOWED_HOSTS = [".vercel.app", "now.sh", ".barakbits.com"]
