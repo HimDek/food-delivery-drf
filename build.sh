acd952464a3cd6f3f71130a8310a96a247faa2aa
Himprakash Deka <himprakashd@gmail.com>
2024-07-18 09:00:39 +0530

    fix

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 rm -rf ./venv/lib/python3.12/site-packages/googleapiclient/discovery_cache/documents/
 pip uninstall pip -y
 du -h -d 1 ./venv/lib/python3.12/site-packages | sort -h
+mkdir staticfiles_build
