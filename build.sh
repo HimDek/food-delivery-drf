de5eefffff8a6429063bb583b9a2ea5dce51e360
Himprakash Deka <himprakashd@gmail.com>
2024-07-18 07:41:19 +0530

    change build script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 rm -rf ./venv/lib/python3.12/site-packages/googleapiclient/discovery_cache/documents/
-pip uninstall pip
+pip uninstall pip -y
 du -h -d 1 ./venv/lib/python3.12/site-packages | sort -h
 python3.12 manage.py collectstatic
