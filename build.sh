f17bfa546ee73b9f2a097547e11eb64ea3eedbc9
Himprakash Deka <himprakashd@gmail.com>
2024-07-18 07:29:44 +0530

    fix build script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ python3.12 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
+rm -rf ./venv/lib/python3.12/site-packages/googleapiclient/discovery_cache/documents/
 python3.12 manage.py collectstatic
